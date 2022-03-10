2d379b1c828993dbd75aa6e4c7d9f74182969d4f
mik284 <mikeykamau222@gmail.com>
2022-03-10 10:05:15 -0800

    new task 6 correction of 4 and 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
- * Main-prints the phrase with proper grammar, but the outcome is a piece of art
+ * Main-prints the phrase with proper grammar, 
+ * but the outcome is a piece of art
+ *
  * Return: 0 if exite properly, non-zero
  */
 int main(void)
