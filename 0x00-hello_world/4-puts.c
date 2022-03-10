fa187a3f45aef8befac589d0c7f517e59240b45a
mik284 <mikeykamau222@gmail.com>
2022-03-10 10:58:59 -0800

    new task 6 correction of 4 and 5 Task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 
 /**
- * main - prints this phrase Programming is like 
+ * main - prints this phrase Programming is like
  * building a Multilingual puzzle.
  *
  * Return: 0 if exited properly, non-zero
-*/ 
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
