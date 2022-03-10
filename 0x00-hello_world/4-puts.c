cd39d93bfb4d8b1a8057ee465864476b5fd0d0c1
mik284 <mikeykamau222@gmail.com>
2022-03-10 07:57:20 -0800

    Added C program that Prints using puts and printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print this phrase "Programming is like  building a Multilingual puzzle
+ * Return (0)
+ */ 
+
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Main task- prints the phrase with proper grammer, but the outcome
+ * is a piece of art
+ * Return: 0 when you exit
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a pice of art,\n");
+	return (0);
+}
