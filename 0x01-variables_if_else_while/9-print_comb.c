0d8139bcb89f76aa2ba5a6c1b00918d4bc51f462
mik284 <mikeykamau222@gmail.com>
2022-03-11 11:05:38 -0800

    Added task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,14 +9,13 @@ int main(void)
 {
 	int n;
 
-	for (n = '0'; n < '10'; n++)
+	for (n = '0'; n <= '9'; n++)
 	{
 		putchar(n);
-		if (n != '9')
-		{
+		if (n == '9')
+		continue;
 			putchar(',');
 			putchar(' ');
-		}
 	}
 	putchar('\n');
 	return (0);
