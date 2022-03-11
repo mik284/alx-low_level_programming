5ac5f6857f2059d8c730b18e9a7a4ebb8d76cd0d
mik284 <mikeykamau222@gmail.com>
2022-03-11 10:56:17 -0800

    Added task 6 and 8 new

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,10 +9,10 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n < 9; n++)
+	for (n = '0'; n <= '9'; n++)
 	{
 		putchar(n);
 	}
-	putchar('10');
+	putchar(10);
 	return (0);
 }
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,14 +10,14 @@ int main(void)
 	int n;
 	char ch;
 	
-	for (n = 0; n < 10; n++)
+	for (n = '0'; n <= '9'; n++)
 	{
 		putchar(n);
 	}
-	for (ch = 'a'; ch >= 'f'; ch++)
+	for (ch = 'a'; ch <= 'f'; ch++)
 	{
 		putchar(ch);
 	}
-	putchar('\n');
+	putchar(10);
 	return (0);
 }
