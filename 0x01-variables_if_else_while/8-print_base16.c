91edd90ed7f16141bdae1f9a8b6f745189562627
mik284 <mikeykamau222@gmail.com>
2022-03-11 10:23:47 -0800

    Added task 8 and 9

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,6 +12,6 @@ int main(void)
 	{
 		putchar(ch);
 	}
-	putchr('\n');
+	putchar('\n');
 	return (0);
 }
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,10 +9,10 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n < 10; n++)
+	for (n = '0'; n < '10'; n++)
 	{
 		putchar(n);
-		if (n != 9)
+		if (n != '9')
 		{
 			putchar(',');
 			putchar(' ');
