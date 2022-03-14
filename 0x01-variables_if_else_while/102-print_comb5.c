ba13f3117175565df26398c60de2bf983e66bd11
mik284 <mikeykamau222@gmail.com>
2022-03-14 08:54:37 -0700

    Added advanced task 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible comb of two
+ * digits number
+ * Return: 0 to show the succes of the program
+ */
+int main(void)
+{
+	int n, m;
+
+	for (i = 0; n < 100; n++)
+	{
+		for (m = 0; m < 100; m++)
+		{
+			if (n < m)
+			{
+				putchar((n / 10) + 48);
+				putchar((n % 10) + 48);
+				putchar(' ');
+				putchar((m / 10) + 48);
+				putchar((m % 10) + 48);
+				if (n != 98 || m != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+}
