1229146bd706e3f1e02c462164de037cc34d2ad7
mik284 <mikeykamau222@gmail.com>
2022-03-15 07:57:49 -0700

    Added new task 1 new

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * main - print putchar followed by
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar("_putchar\n");
 	return (0);
 }
