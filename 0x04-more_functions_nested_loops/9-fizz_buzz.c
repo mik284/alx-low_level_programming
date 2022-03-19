7d5c2dddc821fa01a680a06ae7c095789801b906
Michael Kamau <65639270+mik284@users.noreply.github.com>
2022-03-19 05:13:23 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,35 +1,34 @@
-#include "main.h"
 #include <stdio.h>
-
 /**
- * main - prints the number from 1 to 100 \n
- * but for multiple of 3points fizz intead of the no.
- * Return: 0 for succes
- */
+*main - prints fizz or buzz or fizzbuzz
+*
+*Return: 0
+*/
 int main(void)
 {
 	int i;
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf(" Fizz");
-		} else if (i % 5 == 0 && i % 3 != 0)
-		{
-			printf(" Buzz");
-		} else if (i % 3 == 0 && i % 5 != 0)
-		{
-			printf(" FizzBuzz");
-		} else if (i == 1)
-		{
-			printf("%d", i);
-		} else
-		{
-			printf(" %d", i);
+			printf("FizzBuzz ");
 		}
+		else if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i == 100)
+		{
+			printf("Buzz");
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf("Buzz ");
+		}
+		else
+			printf("%d ", i);
 	}
 	printf("\n");
-
 	return (0);
 }
