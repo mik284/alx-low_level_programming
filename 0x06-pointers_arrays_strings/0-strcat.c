2b8f08ab9a60208135cc7c22ded3abbd74c2f891
mik284 <mikeykamau222@gmail.com>
2022-03-24 09:25:06 -0700

    Added task 1 and the prototype lib

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,17 +13,15 @@ char *_strcat(char *dest, char *src)
 	i = 0;
 	j = 0;
 
-	while (dest[i] != '0')
+	while (dest[i] != '\0')
 		i++;
 
-	while (src[i] != '0')
+	while (src[i] != '\0')
 	{
 		dest[i] = src[j];
 		j++;
 		i++;
 	}
-
 	dest[i] = '\0';
-
 	return (dest);
 }
