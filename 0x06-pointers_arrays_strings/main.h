5b461e7e7253f83513826004bc0b0936ce56408c
mik284 <mikeykamau222@gmail.com>
2022-03-24 10:01:52 -0700

    Added task 2

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,9 +2,9 @@
 #define FILE_MAIN
 
 int _putchar(char c);
-char *_strcat(char *dest, char *str);
-char *_strncat(char *dest, char *str, int n);
-char *_strncpy(char *dest, char *str, int n);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_touppper(char *);
