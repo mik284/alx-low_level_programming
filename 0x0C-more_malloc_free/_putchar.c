8215d7cc1832d128fac63990c83bc1cd088be998
Michael Kamau <65639270+mik284@users.noreply.github.com>
2022-04-08 06:17:09 +0300

    added putchar

--- 0x0C-more_malloc_free/_putchar.c
+++ 0x0C-more_malloc_free/_putchar.c
@@ -1,1 +1,12 @@
-#include
+#include <unistd.h>
+
+/**
+ * _putchar - writes the char c to stdout
+ * @c: The char to print
+ *
+ * Return: 1 for succes
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
