63003d9e704da1e004e0bb17b2955b653393ece5
Michael Kamau <65639270+mik284@users.noreply.github.com>
2022-04-08 05:53:13 +0300

    added updates on task 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -57,34 +57,34 @@ void errors(void)
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+	int num1, num2, mul, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	len = len1 + len2 + 1;
-	result = malloc(sizeof(int) * len);
+	num1 = _strlen(s1);
+	num2 = _strlen(s2);
+	mul = num1 + num2 + 1;
+	result = malloc(sizeof(int) * mul);
 	if (!result)
 		return (1);
-	for (i = 0; i <= len1 + len2; i++)
+	for (i = 0; i <= num1 + num2; i++)
 		result[i] = 0;
-	for (len1 = len1 - 1; len1 >= 0; len1--)
+	for (num1 = num1 - 1; num1 >= 0; num1--)
 	{
-		digit1 = s1[len1] - '0';
+		digit1 = s1[num1] - '0';
 		carry = 0;
-		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		for (num2 = _strlen(s2) - 1; num2 >= 0; num2--)
 		{
-			digit2 = s2[len2] - '0';
-			carry += result[len1 + len2 + 1] + (digit1 * digit2);
-			result[len1 + len2 + 1] = carry % 10;
+			digit2 = s2[num2] - '0';
+			carry += result[num1 + num2 + 1] + (digit1 * digit2);
+			result[num1 + num2 + 1] = carry % 10;
 			carry /= 10;
 		}
 		if (carry > 0)
-			result[len1 + len2 + 1] += carry;
+			result[num1 + num2 + 1] += carry;
 	}
-	for (i = 0; i < len - 1; i++)
+	for (i = 0; i < mul - 1; i++)
 	{
 		if (result[i])
 			a = 1;
