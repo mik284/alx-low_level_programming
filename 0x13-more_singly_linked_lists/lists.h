de1e01e8b788699e931170b3b59ae59edfaeaaa2
Michael Kamau <65639270+mik284@users.noreply.github.com>
2022-04-26 15:16:24 +0300

    added correction

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,6 +4,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for 0x13 C project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
 
 size_t print_listint(const list_t *h);
 
