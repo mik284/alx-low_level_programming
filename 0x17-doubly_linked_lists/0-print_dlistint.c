6db50062ff6ddd3ceb9d193668968ee50b3b05fc
Michael Kamau <65639270+mik284@users.noreply.github.com>
2022-06-11 03:43:28 +0300

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -13,9 +13,9 @@ size_t print_dlistint(const dlistint_t *h)
 
 	if (h == NULL)
 	{
-		printf("%s", "Empty lisit");
+		printf("%s", "Empty list");
 	}
-	while (h != NULL)
+	while (h)
 	{
 		
 		printf("%d\n", h->n);
