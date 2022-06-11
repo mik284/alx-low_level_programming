e67f04d8974cdb4e894e26982d490e5fd6351198
Michael Kamau <65639270+mik284@users.noreply.github.com>
2022-06-11 03:50:43 +0300

    Correction of "1. List length"

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,24 +1,23 @@
 #include "lists.h"
 
 /**
- * dlistint_len - a function that returns the number
- * of elements in a linked dlistint_t list
- * @h: head node
+ * dlistint_len - returns the number of nodes in a doubly linked list
+ * @h: pointer to the list
  *
- * Return: lenght of elements
+ * Return: number of nodes
  */
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t nodes = 0;
 
-	if (h == NULL)
-	{
-		printf("%s", "Empty list");
-	}
-	while (h != NULL)
+	if (!h)
+		return (0);
+
+	while (h)
 	{
 		nodes++;
 		h = h->next;
 	}
+
 	return (nodes);
 }
